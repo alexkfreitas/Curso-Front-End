df16471374e2c08bb44882b88916c4f31722f3b4
Alex Katanosaka Freitas <63432377+alexkfreitas@users.noreply.github.com>
2020-04-14 01:52:16 +0000

    Primeiro projeto do curso de Front End

---
+++ javascript.js
@@ -0,0 +1,42 @@
+// exercício 1
+
+console.log("EXERCÍCIO 1")
+
+let nome = "Alex";
+let idade = 21;
+let altura = 1.60;
+
+console.log("A altura é",altura);
+
+// exercício 2
+
+console.log("EXERCÍCIO 2")
+idade = idade + (idade * (10/100))
+console.log("A idade + 10% é",idade);
+
+
+// exercício 3
+
+console.log("EXERCÍCIO 3")
+ 
+let c = 30;
+fah = 9*(c/5)+32
+console.log("O valor em fahrenheit da temperatura 30 é",fah);
+
+cel = 5*(fah-32)/9
+console.log("O valor em celsius é",cel);
+
+// exercício 4
+
+console.log("EXERCÍCIO 4")
+
+a = 3
+b = 4
+hipo = Math.sqrt((a**2) + (b**2))
+console.log("A hipotenusa é",hipo);
+
+
+// exercicio 5
+
+console.log("EXERCÍCIO 5")
+
